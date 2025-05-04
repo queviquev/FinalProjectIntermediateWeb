ea5ae7343f66956fffd86a19fbb25647fcad4fc5
queviquev <166847810+queviquev@users.noreply.github.com>
2025-05-04 15:13:00 -0500

    Delete src/src directory

--- src/src/scripts/utils.js
+++
@@ -1,6 +0,0 @@
-function collision({object1, object2}) {
-    return(
-        // Check if the player is falling or jumping through the block
-        object1.position.y + object1.height >= object2.position.y && object1.position.y + object2.position.y + object2.position.height && object1.position.x <= object2.position.x + object2.width && object1.position.x + object1.width >= object2.position.x
-    ) 
-}
